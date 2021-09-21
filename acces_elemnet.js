a0f290f11bec53f5ea35b89529f33df5199523de
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-21 14:02:31 +0530

    Add files via upload

---
+++ acces_elemnet.js
@@ -0,0 +1,11 @@
+// var marks=["rubi","aaa",1,2,3,4]
+// console.log(marks[4]);
+
+
+
+// with loop
+
+var marks=[1,2,3,4,5]
+for (var index=0; index<marks.length;index++){
+    console.log(index)
+}
---
+++ ques7.js
@@ -0,0 +1,2 @@
+var numbers=[50, 40, 23, 70, 56, 12, 5, 10, 7,5,6,7,8,9,9,9,9]
+console.log(numbers.length);
