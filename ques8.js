f98ed6004409a9acb2f10165b742df6ee7061178
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-21 14:36:59 +0530

    Add files via upload

---
+++ ques8.js
@@ -0,0 +1,18 @@
+var Numbers=[75, 40, 23, 70, 56, 8, 5, 10, 7]
+var l=Numbers.length
+var max = 0
+var i=0
+for (i = 0; l > i; i++) {
+
+    if (Numbers[i] > max) {
+
+        max = Numbers[i];
+
+    }
+
+}
+
+console.log(max);
+
+
+
