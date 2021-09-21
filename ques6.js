fd39b5889a599ad85df8866a6c36f8c9d25af8d7
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-21 12:22:54 +0530

    Add files via upload

---
+++ ques6.js
@@ -0,0 +1,13 @@
+
+// var mygame = ["chess", "Ludo", "Badminton", "Basketball", "Carom", "Cricket"];
+
+// var delet = mygame.indexOf("Basketball");//get  "Basketball" index
+
+// mygame.splice(delet, 1);
+
+// console.log(mygame)
+
+var g=[4,5,6,7,8,9]
+var s=g.indexOf(9)
+g.splice(s,1)
+console.log(g)
