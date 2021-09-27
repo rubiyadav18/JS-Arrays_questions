e9ec249fe564f4f0b006568e3950849c7c9a2ee6
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-27 18:26:22 +0530

    Add files via upload

---
+++ ques18.js
@@ -0,0 +1,51 @@
+// var elements = [23, 14, 56, 12, 19, 9, 15, 25, 31];
+// var count_odd_numbers=0;
+// var sum_odd_numbers=0;
+// var sum_even_numbers=0;
+// var count_even_numbers=0;
+// for ( i of elements){
+//     if (i%2==0){
+//         sum_even_numbers+i;
+//         count_even_numbers+i;
+
+//     }
+//     else{
+//         sum_odd_numbers+i;
+//         count_odd_numbers+i;
+
+//     }   
+// }
+// console.log(sum_even_numbers+sum_odd_number)/2);
+// console.log(sum_even_numbers);
+// console.log(count_even_numbers);
+// console.log(sum_odd_numbers);
+// console.log(count_odd_numbers);
+
+var a=[1,2,3,4]
+var b=[1,3,3,4]
+
+console.log(a==b)
+
+
+
+
+
+
+// var a=[1,2,3];
+// var b=[1,2,3];
+// var a = b;
+// console.log(a==b)
+// console.log(typeof(a))
+// if (JSON.stringify(a)==JSON.stringify(b)){
+//     console.log(typeof(a))
+// }else{
+//     console.log(false)
+// }
+
+
+
+
+
+// console.log(JSON.stringify(a===b)
+
+
